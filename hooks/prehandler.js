589a857a9492f5592838e4467ea139500199d2bd
manutd <dev@localhost>
2021-04-15 11:43:52 +0700

    add-sleep-func

--- hooks/prehandler.js
+++ hooks/prehandler.js
@@ -1,21 +1,10 @@
 const { getToken } = require('../repos/token.repo')
 const { ts } = require('../helpers/string')
 const { NO_TOKEN, INVALID_TOKEN } = require('../configs/constant').ERRORS
-// const { tracer, opentracing } = require('../helpers/logger.jaeger')
 
 exports.verifyToken = async (req, res) => {
-  /*
-  const span = tracer.startSpan('http_request')
-  span.addTags({
-    [opentracing.Tags.SPAN_KIND]: opentracing.Tags.SPAN_KIND_MESSAGING_PRODUCER,
-    [opentracing.Tags.HTTP_METHOD]: req.method,
-    [opentracing.Tags.HTTP_URL]: req.url
-  })
-  span.setTag(opentracing.Tags.HTTP_STATUS_CODE, res.statusCode)
-  */
-
   const reqToken = req.headers.token
-  if (process.env.NODE_ENV === 'localhost') return true
+  // if (process.env.NODE_ENV === 'localhost') return true
 
   if (!reqToken) {
     res.code(401).send({
@@ -34,12 +23,4 @@ exports.verifyToken = async (req, res) => {
       err_code: INVALID_TOKEN.code
     })
   }
-
-  // span.finish()
-  // req.span = span
-
-  // assign something to req object here
-  // req.token = token; // then try to print on controller handler
-  // let tokenWithUser = await getTokenWithUser({ token: reqToken });
-  // req.tokenWithUser = tokenWithUser;
 }
